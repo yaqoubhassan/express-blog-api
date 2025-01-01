3808a1ca502648bc83340ff2d58c0f39f069a819
Yakubu Alhassan <yaqoubdramani@gmail.com>
2025-01-01 15:43:15 +0000

    Create comment model

---
+++ models/commentModel.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const commentSchema = new mongoose.Schema({
+  content: { type: String, require: [true, "The content field is required"] },
+  post: { type: mongoose.Schema.Types.ObjectId, ref: "Post", required: true },
+  author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  createdAt: { type: Date, default: Date.now() },
+});
+
+module.exports = mongoose.model("Comment", commentSchema);
