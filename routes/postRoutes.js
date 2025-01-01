f68ed866630e87d3de57b19dd520750eab41a671
Yakubu Alhassan <yaqoubdramani@gmail.com>
2025-01-01 19:26:00 +0000

    Add endpoint for listing posts

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { create } = require("../controllers/postController");
+const { create, index } = require("../controllers/postController");
 const { authMiddleware } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 router.post("/", authMiddleware, create);
+router.get("/", index);
 
 module.exports = router;
