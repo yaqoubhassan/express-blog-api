97b55338d464290312455875d1a6ba6f25f94f0d
Yakubu Alhassan <yaqoubdramani@gmail.com>
2025-01-01 20:22:48 +0000

    Add endpoint for updating post

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,5 +1,10 @@
 const express = require("express");
-const { create, index, show } = require("../controllers/postController");
+const {
+  create,
+  index,
+  show,
+  update,
+} = require("../controllers/postController");
 const { authMiddleware } = require("../middleware/authMiddleware");
 
 const router = express.Router();
@@ -7,5 +12,6 @@ const router = express.Router();
 router.post("/", authMiddleware, create);
 router.get("/", index);
 router.get("/:id", show);
+router.patch("/:id", authMiddleware, update);
 
 module.exports = router;
