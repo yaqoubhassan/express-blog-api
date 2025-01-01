d10195af7a360852c9961c6875cecae553aae3ec
Yakubu Alhassan <yaqoubdramani@gmail.com>
2025-01-01 20:01:17 +0000

    Add endpoint for fetching a single post

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { create, index } = require("../controllers/postController");
+const { create, index, show } = require("../controllers/postController");
 const { authMiddleware } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 router.post("/", authMiddleware, create);
 router.get("/", index);
+router.get("/:id", show);
 
 module.exports = router;
