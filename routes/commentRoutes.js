d10f690544c2bd1540982b5e20b160102dab81c8
Yakubu Alhassan <yaqoubdramani@gmail.com>
2025-01-02 18:51:54 +0000

    Add endpoints for updating and deleting comment

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -1,10 +1,17 @@
 const express = require("express");
-const { store, index } = require("../controllers/commentController");
+const {
+  store,
+  index,
+  update,
+  destroy,
+} = require("../controllers/commentController");
 const { authMiddleware } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 router.post("/:postId", authMiddleware, store);
 router.get("/:postId", index);
+router.patch("/:commentId", authMiddleware, update);
+router.delete("/:commentId", authMiddleware, destroy);
 
 module.exports = router;
