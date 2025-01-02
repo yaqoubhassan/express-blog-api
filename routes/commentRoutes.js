0f7b5b463ace2bcb5a544f9da72a96f55ef7ccbc
Yakubu Alhassan <yaqoubdramani@gmail.com>
2025-01-02 07:27:04 +0000

    Add endpoint for listing comments on a post

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { store } = require("../controllers/commentController");
+const { store, index } = require("../controllers/commentController");
 const { authMiddleware } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 router.post("/:postId", authMiddleware, store);
+router.get("/:postId", index);
 
 module.exports = router;
