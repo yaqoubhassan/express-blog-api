99389dc970a143e1bfee4a0765a76f81cd6a3ca0
Yakubu Alhassan <yaqoubdramani@gmail.com>
2025-01-02 23:31:05 +0000

    refactor

--- models/commentModel.js
+++ models/commentModel.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const commentSchema = new mongoose.Schema({
   content: { type: String, required: [true, "The content field is required"] },
-  post: { type: mongoose.Schema.Types.ObjectId, ref: "Post", required: true },
   author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  post: { type: mongoose.Schema.Types.ObjectId, ref: "Post", required: true },
   createdAt: { type: Date, default: Date.now() },
 });
 
--- models/postModel.js
+++ models/postModel.js
@@ -4,8 +4,8 @@ const postSchema = new mongoose.Schema({
   title: { type: String, required: [true, "The title field is required"] },
   content: { type: String, required: [true, "The content field is required"] },
   author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  createdAt: { type: Date, default: Date.now() },
   comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
+  createdAt: { type: Date, default: Date.now() },
 });
 
 module.exports = mongoose.model("Post", postSchema);
