e3116bad4f6fd723c6fe9432dcf1750f7055021a
Yakubu Alhassan <yaqoubdramani@gmail.com>
2025-01-02 06:43:47 +0000

    Fix typo

--- models/commentModel.js
+++ models/commentModel.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const commentSchema = new mongoose.Schema({
-  content: { type: String, require: [true, "The content field is required"] },
+  content: { type: String, required: [true, "The content field is required"] },
   post: { type: mongoose.Schema.Types.ObjectId, ref: "Post", required: true },
   author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   createdAt: { type: Date, default: Date.now() },
