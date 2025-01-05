74cb9c7edd9023ac649336df8e80c15ec494bbd8
Yakubu Alhassan <yaqoubdramani@gmail.com>
2025-01-05 13:52:37 +0000

    Multer config

---
+++ middleware/multerConfig.js
@@ -0,0 +1,24 @@
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 5 * 1024 * 1024 },
+  fileFilter: function (req, file, cb) {
+    if (file.mimetype.startsWith("image/")) {
+      cb(null, true);
+    } else {
+      cb(new Error("File must be an image"), false);
+    }
+  },
+});
+
+module.exports = upload;
