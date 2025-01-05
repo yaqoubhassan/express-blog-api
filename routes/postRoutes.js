b48eb875f4ac127ff2254e101a521ed9a3a6907e
Yakubu Alhassan <yaqoubdramani@gmail.com>
2025-01-05 08:09:11 +0000

    Add swagger annotation to post listing endpoint

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -11,6 +11,29 @@ const { cacheMiddleware } = require("../middleware/cacheMiddleware");
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * /api/posts:
+ *   get:
+ *     summary: Get all posts
+ *     description: Retrieve a list of all posts
+ *     tags:
+ *       - Posts
+ *     responses:
+ *       200:
+ *         description: Successfully fetched posts
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   title:
+ *                     type: string
+ *                   content:
+ *                     type: string
+ */
 router.post("/", authMiddleware, store);
 router.get("/", cacheMiddleware, index);
 router.get("/:id", show);
